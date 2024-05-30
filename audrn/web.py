36a74844719798963dd050e345f36f5c20f6c225
MyeongGuJo <whaudrn3708@hanyang.ac.kr>
2024-05-31 02:33:13 +0900

    web

--- audrn/web.py
+++ audrn/web.py
@@ -1,6 +1,19 @@
 import streamlit as st
 import yt_dlp
 
+def my_hook(d):
+    if d['status'] == 'downloading':
+        # 진행 상태를 퍼센트로 계산
+        p = d['_percent_str']
+        m_index = p.find('m')
+        percent_index = p.find('%')
+        p = p[m_index+1:percent_index]
+        st.text(p)
+        progress_bar.progress(float(p)/100, "Downloading...")
+    if d['status'] == 'finished':
+        st.success('다운로드 완료!')
+        st.video('videos/downloaded_video.mkv')
+
 st.title('YouTube 동영상 다운로더')
 
 # 사용자로부터 YouTube URL 입력받기
@@ -15,10 +28,12 @@ if st.button('동영상 다운로드'):
         ydl_opts = {
             'format': 'best[height=720]/best',
             'outtmpl': 'videos/downloaded_video.mkv',  # 다운로드 파일 이름
+            'progress_hooks': [my_hook],  # 진행 상태 콜백 함수 설정
         }
         
         # 동영상 다운로드 시도
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            progress_bar = st.progress(0, text="Downloading...")
             try:
                 ydl.download([video_url])
                 st.success('다운로드 완료!')
